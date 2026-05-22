7d83ef0b05715e1876c761177123e8c77a666fe4
LeoMdz <mendoza.roman.leonardo.cb56@gmail.com>
2026-05-21 23:41:17 -0600

    Fix: Tailwind v4 compatibility and next.config deprecation warnings

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['example.com'],
     remotePatterns: [
       {
         protocol: 'https',
@@ -11,7 +10,6 @@ const nextConfig = {
   },
   compress: true,
   reactStrictMode: true,
-  swcMinify: true,
 };
 
 module.exports = nextConfig;
